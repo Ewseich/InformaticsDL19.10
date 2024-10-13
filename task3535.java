7b77e1aa92c4a5ef98a1ad56bae889902f2b8ae8
Ewseich <timapoton@gmail.com>
2024-10-13 15:16:11 +0300

    Initial commit

---
+++ task3535.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class task3535 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print("   _~_    ");
+        }
+        System.out.println("");
+        for (int i = 0; i < n; i++) {
+            System.out.print("  (o o)   ");
+        }
+        System.out.println("");
+        for (int i = 0; i < n; i++) {
+            System.out.print(" /  V  \\  ");
+        }
+        System.out.println("");
+        for (int i = 0; i < n; i++) {
+            System.out.print("/(  _  )\\ ");
+        }
+        System.out.println("");
+        for (int i = 0; i < n; i++) {
+            System.out.print("  ^^ ^^   ");
+        }
+        System.out.println("");
+
+    }
+}
