21c8abf1fb8f37eb8e39352832442861a3e1231b
Ewseich <timapoton@gmail.com>
2024-10-13 15:21:32 +0300

    112204

--- task112204.java
+++ task112204.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
 public class task112204 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < n + 1; i++) {
+            System.out.println(i * 2);
+        }
+
+    }
 }
